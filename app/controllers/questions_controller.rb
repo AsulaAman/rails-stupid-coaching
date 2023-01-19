d1fea014ed1275fba65250c2b3a697df2582f1b3
Asula Aman <asulaaman@gmail.com>
2023-01-19 19:31:52 +0000

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,22 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = answer
+    # POST parameters coming from an HTML form that the user has submitted
+    @question = params[:question]
+    super
+  end
+
+  private
+
+  def coach_answer(question)
+    if question.downcase == 'I am going to work'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
 end
